7084c838a707add0a42ebcef55b65a8d4cc3fdbf
ctasbihas <ctasbihas@gmail.com>
2024-01-11 15:38:14 +0600

    navbar responsive

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -3,32 +3,25 @@ import "./Navbar.css";
 
 const Navbar = () => {
 	return (
-		<section className="flex justify-between items-center py-7 px-16">
-			<Image
-				src="/assets/images/logo.png"
-				alt=""
-				width={130}
-				height={130}
-				className="hover:-translate-y-2 transition duration-500 cursor-pointer"
-			/>
-			<nav>
+		<section className="flex justify-between items-center pt-5 px-5 md:pt-2 lg:px-2 lg:py-7 max-w-screen-md lg:max-w-full xl:max-w-screen-xl mx-auto">
+			<div className="relative w-32 h-7 md:w-32">
+				<Image
+					src="/assets/images/logo.png"
+					alt=""
+					fill
+					className="hover:-translate-y-2 transition duration-500 cursor-pointer"
+				/>
+			</div>
+			<nav className="hidden lg:block">
 				<ul className="flex gap-10">
 					<li className="nav-link">HOME</li>
 					<li className="nav-link">ABOUT</li>
 					<li className="nav-link">MENU</li>
-					<li className="flex gap-2 nav-link">
-						PAGES ∨{" "}
-						{/* <Image
-							src="/assets/icons/chevronDown.svg"
-							alt=""
-							width={15}
-							height={15}
-						/> */}
-					</li>
+					<li className="nav-link">PAGES ∨</li>
 					<li className="nav-link">CONTACT</li>
 				</ul>
 			</nav>
-			<div className="flex gap-10">
+			<div className="flex gap-6">
 				<Image
 					src="/assets/icons/search.svg"
 					alt=""

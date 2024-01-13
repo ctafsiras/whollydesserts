7cc6256dfd333ab430ebd7e46affb28ff0080642
ctasbihas <ctasbihas@gmail.com>
2024-01-13 10:30:45 +0600

    drawer added only for mobile

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -11,7 +11,7 @@ const Drawer = ({
 	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 	return (
 		<main
-			className={`fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ${
+			className={`block lg:hidden fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ${
 				isOpen
 					? "transition-opacity opacity-100 duration-500 translate-x-0 "
 					: "transition-all delay-500 opacity-0 translate-x-full"

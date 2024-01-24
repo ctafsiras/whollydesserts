676f74e4d761879e6bbb35f3f0e0c39328d47256
ctasbihas <ctasbihas@gmail.com>
2024-01-24 21:06:47 +0600

    auth redirect added

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -4,25 +4,24 @@ import LoginForm from "@/components/Authentication/LoginForm";
 import RegisterForm from "@/components/Authentication/RegisterForm";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Auth() {
 	const [authMethod, setAuthMethod] = useState("login");
-	const { status } = useSession();
+	const session = useSession();
 	const router = useRouter();
 
-	if (status === "unauthenticated") {
-		return (
-			<main>
-				{authMethod === "login" ? (
-					<LoginForm changeMethod={() => setAuthMethod("register")} />
-				) : (
-					<RegisterForm changeMethod={() => setAuthMethod("login")} />
-				)}
-			</main>
-		);
-	} else {
-		router.back();
-		return;
-	}
+	useEffect(() => {
+		if (session?.status === "authenticated") router.back();
+	}, [session?.status, router]);
+
+	return (
+		<main>
+			{authMethod === "login" ? (
+				<LoginForm changeMethod={() => setAuthMethod("register")} />
+			) : (
+				<RegisterForm changeMethod={() => setAuthMethod("login")} />
+			)}
+		</main>
+	);
 }

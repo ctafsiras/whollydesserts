7dfc304eb2f33f79165168e78f8844bf0ce78d42
ctasbihas <ctasbihas@gmail.com>
2024-02-12 21:26:27 +0600

    title changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,36 +3,39 @@ import Navbar from "@/components/Navbar/Navbar";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import type { Metadata } from "next";
 import { Abhaya_Libre } from "next/font/google";
+import ToasterContext from "./context/ToasterContext";
 import "./globals.css";
 import { Providers } from "./providers";
-import ToasterContext from "./context/ToasterContext";
 
 const abhayaLibre = Abhaya_Libre({
-  subsets: ["latin"],
-  weight: ["400", "700"],
+	subsets: ["latin"],
+	weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
-  title: "Wholly Desserts | Home",
-  description: "Wholly Desserts Official Web Application",
+	title: "Wholly Desserts",
+	description: "Wholly Desserts Official Web Application",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={abhayaLibre.className}>
-      <body>
-        <Providers>
-          <Navbar />
-          <ToasterContext />
-          {children}
-          <NewsLetter />
-          <Footer />
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={abhayaLibre.className}
+		>
+			<body>
+				<Providers>
+					<Navbar />
+					<ToasterContext />
+					{children}
+					<NewsLetter />
+					<Footer />
+				</Providers>
+			</body>
+		</html>
+	);
 }
